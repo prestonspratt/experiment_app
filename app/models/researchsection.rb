840144aaf573b48cad0b0c2fd8d7ae7449f8ae5f
Preston Spratt <prestonspratt@gmail.com>
2013-01-22 10:53:10 -0500

    Only one research section per experiment

--- app/models/researchsection.rb
+++ app/models/researchsection.rb
@@ -2,5 +2,6 @@ class Researchsection < ActiveRecord::Base
   attr_accessible :content, :experiment_id, :videolink
   belongs_to :experiment
 
-  validates :experiment_id, presence: true
+  validates :experiment_id, presence: true,
+  						              uniqueness: true
 end
